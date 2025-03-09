877886b24c3040257a8debd7edd7f34d0f16dc95
Kazuki Fujimaru <kazukifujimaru@gmail.com>
2025-03-09 21:39:31 +0700

    Add: Testing page content
    - Penambahan style page content
    - Penambahan test page content

--- handlers/routes.py
+++ handlers/routes.py
@@ -5,3 +5,7 @@ def configure_routes(app):
     @app.route("/")
     def index():
         return render_template("index.html")
+    
+    @app.route("/testpg")
+    def testpg():
+        return render_template("testpg.html")
