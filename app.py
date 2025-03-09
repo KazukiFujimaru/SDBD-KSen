5653c3b6d64ff22f73d6f1781bcc75434bafaa52
Kazuki Fujimaru <kazukifujimaru@gmail.com>
2025-03-09 15:24:08 +0700

    rev: Perbaikan deployment
    Deploy test di vercel #2

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask
+from handlers.routes import configure_routes
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+configure_routes(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
